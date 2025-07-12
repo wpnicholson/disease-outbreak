66a0654e0021fb507bd68a34fef53523943566e9
William Nicholson <42596564+wpnicholson@users.noreply.github.com>
2025-07-11 22:31:05 -0600

    Improve environment variable loading in database.py
    Load environment variables from .env file only if not in production and handle missing dotenv module gracefully. Added a check to ensure DATABASE_URL is set, raising an error if not.

--- app/database.py
+++ app/database.py
@@ -2,11 +2,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+if os.getenv("ENV") != "prod":
+    try:
+        from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+        load_dotenv()
+    except ImportError:
+        print(
+            "Warning: dotenv module not found. Environment variables may not be loaded."
+        )
+
+DATABASE_URL: str | None = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set.")
 
 engine = create_engine(DATABASE_URL)
 
